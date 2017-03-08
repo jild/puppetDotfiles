b488d008eb60a35e6dc678e954975182bb98501c
mm <mm@manjaro0815.local>
2017-03-08 15:50:04 +0100

    added nvim replaces vi commands

---
+++ do.sh
@@ -0,0 +1,5 @@
+sudo ln -s /usr/bin/nvim /usr/bin/vi                                                   │~                              |~                                                               
+sudo ln -s /usr/bin/nvim /usr/bin/vim                                                  │~                              |~                                                               
+sudo /usr/bin/vim /usr/bin/vim-old                                                     │~                              |~                                                               
+sudo mv /usr/bin/vim /usr/bin/vim-old
+sudo ln -s /usr/bin/nvim /usr/bin/vim 
